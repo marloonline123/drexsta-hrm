c68a7e762d52f447c9fab75e8687d90bc9aa14e2
Abotalib Adam <abotalib@abotalib.com>
2025-09-03 16:00:14 +0300

    feat(migrations): add username and profile photo fields to users table. Create companies and company_user migrations to manage companies.

---
+++ database/migrations/2025_09_03_122653_create_companies_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique()->index();
+            $table->string('phone')->nullable();
+            $table->string('email')->nullable()->unique();
+            $table->string('address')->nullable();
+            $table->text('description')->nullable();
+            $table->string('logo_path')->nullable();
+            $table->boolean('is_active')->default(1);
+            $table->timestamps();
+            $table->softDeletes();
+
+            // Indexes
+            $table->index(['name', 'industry', 'is_active']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+};
